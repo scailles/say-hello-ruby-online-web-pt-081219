79ffbe64ba64d8c7e807b90b99547e389d353346
celebrated-integer-2023 <scailles23@gmail.com>
2019-08-19 03:04:37 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,6 @@
 # Build your say_hello method here
-def say_hello("Ruby Programmer")
-  puts "Hello #{"Ruby Programmer"}"
+def say_hello(name)
+  puts "Hello #{name}!"
+else 
+  puts "Hello Ruby Programmer!"
 end
